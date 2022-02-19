9eced42ecc499d6e6d69d1df3b14472299029ea6
danjhkim <danjhkim@gmail.com>
2022-02-18 21:22:05 -0500

    attempt to netlify

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,14 @@
 const Welcome = () => {
-	return <h3>Welcome! Sign up or sign in!</h3>;
+	return (
+		<div>
+			<h3>Welcome! Sign up or sign in!</h3>
+			<span>Note: THINGS TO ADD</span>
+			<ul>
+				<li>email confirmation</li>
+				<li>retrieve forgotten password</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Welcome;
